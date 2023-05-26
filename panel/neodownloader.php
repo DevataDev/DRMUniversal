d45bf99872d8b463b2c68e2ae14a7c3ad33bbeda
Devata <devatadev@pm.me>
2023-05-27 00:09:37 +0800

    try to use fifo

--- panel/neodownloader.php
+++ panel/neodownloader.php
@@ -815,7 +815,8 @@ function WriteSegment($ChID, $ChName, $Keys, $aHeader, $aData, $vHeader, $vData,
     // $cmd = $FFMpegBin . " -copyts " . $MyFFMpegCMD . $Redirect;
 
     //$cmd=$FFMpegBin." -hide_banner -start_at_zero -correct_ts_overflow 0 -avoid_negative_ts disabled -max_interleave_delta 0 -i $VideoDecFileName $strAudioIn -map 0:v $map -c:v copy -c:a copy $Merged_FileName";
-    stream_set_blocking($Merged_Fifo, 0);
+    // stream_set_blocking($Merged_Fifo, 0);
+    StartFFMPEG($ChName, $ChID, $audioCount);
     $cmd = $FFMpegBin . " -hide_banner -fflags +igndts -copyts -i $VideoDecFileName $strAudioIn -map 0:v $map -c:v copy -c:a copy -f mpegts $namedPipe";
     DoLog("Merging Command : $cmd");
     DoLog("Merging segment .... please wait .....");
@@ -870,7 +871,7 @@ function WriteSegment($ChID, $ChName, $Keys, $aHeader, $aData, $vHeader, $vData,
         unlink($VideoDecFileName);
     }
 
-    StartFFMPEG($ChName, $ChID, $audioCount);
+    
 }
 
 function CreateFifo($ChName, $FileName)
