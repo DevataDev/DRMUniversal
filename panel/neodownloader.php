5a9cd7a40346afe0cbefabe7af25a05355e56c1d
Devata <devatadev@pm.me>
2023-05-27 00:14:45 +0800

    try to use fifo

--- panel/neodownloader.php
+++ panel/neodownloader.php
@@ -830,17 +830,17 @@ function WriteSegment($ChID, $ChName, $Keys, $aHeader, $aData, $vHeader, $vData,
     fwrite($Merged_Fifo, implode("", $Res));
     DoLog("Merging segment done");
 
-    if ($CheckKey) {
-        $cmd = "ffmpeg -v error -i $namedPipe -f null - > $WorkPath/$ChName/log/checkkey.txt 2>&1";
-        exec($cmd);
-        $Err = file_get_contents("$WorkPath/$ChName/log/checkkey.txt");
-        if (strpos($Err, "error while decoding") === false) {
-            //ok
-        } else {
-            UpdateChanStatus2($ChID, "KeyError");
-            die();
-        }
-    }
+    // if ($CheckKey) {
+    //     $cmd = "ffmpeg -v error -i $namedPipe -f null - > $WorkPath/$ChName/log/checkkey.txt 2>&1";
+    //     exec($cmd);
+    //     $Err = file_get_contents("$WorkPath/$ChName/log/checkkey.txt");
+    //     if (strpos($Err, "error while decoding") === false) {
+    //         //ok
+    //     } else {
+    //         UpdateChanStatus2($ChID, "KeyError");
+    //         die();
+    //     }
+    // }
 
     $cmd = "ffprobe -v quiet -print_format json -show_streams -show_format $namedPipe > a.json";
     exec($cmd);
