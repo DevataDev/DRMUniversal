cd86b6cff723f1ec4ea395e975ad83d3f7dc030d
Devata <devatadev@pm.me>
2023-05-27 00:24:51 +0800

    try to use fifo

--- panel/neodownloader.php
+++ panel/neodownloader.php
@@ -842,7 +842,7 @@ function WriteSegment($ChID, $ChName, $Keys, $aHeader, $aData, $vHeader, $vData,
         }
     }
 
-    $cmd = "ffprobe -v quiet -print_format json -show_streams -show_format $Merged_Fifo > a.json";
+    $cmd = "ffprobe -v quiet -print_format json -show_streams -show_format $namedPipe > a.json";
     exec($cmd);
     $v = json_decode(file_get_contents("a.json"), true);
     DoLog("Video Info - $cmd - : " . json_encode($v));
